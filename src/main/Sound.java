61754bd8788c063b19baa192d4347b0b28226613
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2025-12-04 10:22:25 +0100

    Started working on sounds

---
+++ src/main/Sound.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.sound.sampled.Clip;
+import java.net.URL;
+
+public class Sound {
+
+    Clip clip;
+    URL soundURL[] = new URL[30];
+
+    public Sound() {
+        soundURL[0] = getClass().getResource("/sound/");
+    }
+}
