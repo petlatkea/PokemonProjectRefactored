911d2d2ab63800aca917bc06e31f4329e6a0427f
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2025-12-10 11:18:32 +0100

    added more pokeballs and NPCs

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -15,11 +15,31 @@ public class AssetSetter {
         gp.obj[0].worldX = 18 * gp.tileSize;
         gp.obj[0].worldY = 35 * gp.tileSize;
         gp.obj[0].collision = true;
+
+        gp.obj[1] = new OBJ_Pokeball(gp);
+        gp.obj[1].worldX = 41 * gp.tileSize;
+        gp.obj[1].worldY = 5 * gp.tileSize;
+        gp.obj[1].collision = true;
+
+        gp.obj[2] = new OBJ_Pokeball(gp);
+        gp.obj[2].worldX = 91 * gp.tileSize;
+        gp.obj[2].worldY = 31 * gp.tileSize;
+        gp.obj[0].collision = true;
+
+        gp.obj[3] = new OBJ_Pokeball(gp);
+        gp.obj[3].worldX = 30 * gp.tileSize;
+        gp.obj[3].worldY = 63 * gp.tileSize;
+        gp.obj[3].collision = true;
     }
 
     public void setNPC() {
         gp.npc[0] = new NPC_Female1(gp);
         gp.npc[0].worldX = 23 * gp.tileSize;
         gp.npc[0].worldY = 39 * gp.tileSize;
+
+        gp.npc[1] = new NPC_Female1(gp);
+        gp.npc[1].worldX = 41 * gp.tileSize;
+        gp.npc[1].worldY = 11 * gp.tileSize;
+
     }
 }
