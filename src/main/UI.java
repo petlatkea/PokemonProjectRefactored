fed6597c4560fd7f0df61c4befb5db8ae05b55c6
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2025-12-11 20:58:44 +0100

    added dialogue UI & gameState handling

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,24 +1,37 @@
 package main;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class UI {
     GamePanel gp;
     Graphics2D g2;
 
-    Font arial_40, arial_80B;
+    UtilityTool uTool = new UtilityTool();
+    public BufferedImage dialogueWindowImage;
+
+    Font pkmnFont;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
-    double playTime;
-    DecimalFormat dFormat = new DecimalFormat("#0.00");
+    public String currentDialogue = "";
 
     public UI (GamePanel gp) {
         this.gp = gp;
 
-        arial_40 = new Font("Arial", Font.PLAIN, 40);
-        arial_80B = new Font("Arial", Font.BOLD, 80);
+        InputStream is = getClass().getResourceAsStream("/font/pkmnFont.ttf");
+        try {
+            pkmnFont = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        getUIImage();
     }
 
     public void showMessage(String text) {
@@ -26,17 +39,34 @@ public class UI {
         messageOn = true;
     }
 
+    public void getUIImage() {
+        try {
+            dialogueWindowImage = ImageIO.read(getClass().getResourceAsStream("/ui/dialogueBox.png"));
+            uTool.scaleImage(dialogueWindowImage, 3, 3);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void draw(Graphics2D g2) {
         this.g2 = g2;
 
-        g2.setFont(arial_40);
+        g2.setFont(pkmnFont);
         g2.setColor(Color.white);
 
+        // PLAY STATE
         if (gp.gameState == gp.playState) {
 
         }
+
+        // PAUSE STATE
         if (gp.gameState == gp.pauseState) {
             drawPauseScreen();
+        }
+
+        // DIALOGUE STATE
+        if (gp.gameState == gp.dialogueState) {
+            drawDialogueScreen();
         }
     }
 
@@ -48,6 +78,27 @@ public class UI {
         int x = getXForCenteredText(text);
         int y = gp.screenHeight/2;
         g2.drawString(text, x, y);
+    }
+
+    public void drawDialogueScreen() {
+        // WINDOW
+        int x = (gp.screenWidth - (dialogueWindowImage.getWidth() * 4)) / 2;
+        int y = gp.screenHeight - (dialogueWindowImage.getHeight() * 4) - (gp.tileSize / 8);
+        drawDialogueWindow(x, y, dialogueWindowImage);
+
+        // TEXT
+        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 26));
+        x += gp.tileSize-5;
+        y += gp.tileSize+10;
+        g2.setColor(Color.black);
+        for (String line : currentDialogue.split("\n")) {
+            g2.drawString(line, x, y);
+            y += 55;
+        }
+    }
+
+    public void drawDialogueWindow(int x, int y, BufferedImage image) {
+        g2.drawImage(image, x, y, image.getWidth()*4, image.getHeight()*4, null);
     }
 
     public int getXForCenteredText(String text) {
