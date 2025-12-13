1efbf671c1113377684bb580f2fccc3f3cd2c636
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2025-12-13 21:43:46 +0100

    code simplification

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -44,7 +44,6 @@ public class Entity {
             case "down" -> direction = "up";
             case "left" -> direction = "right";
             case "right" -> direction = "left";
-
         }
     }
     public void update() {}
