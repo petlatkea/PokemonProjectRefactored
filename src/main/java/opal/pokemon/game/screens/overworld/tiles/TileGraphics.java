aee6743c9cfc1fbede33da5709cb3aaf065f0b60
Peter Lind <petlatkea@peterlind.dk>
2026-01-16 21:10:08 +0100

    removed debugging from TileGraphics

--- src/main/java/opal/pokemon/game/screens/overworld/tiles/TileGraphics.java
+++ src/main/java/opal/pokemon/game/screens/overworld/tiles/TileGraphics.java
@@ -2,7 +2,6 @@ package main.java.opal.pokemon.game.screens.overworld.tiles;
 
 import main.java.opal.pokemon.game.ViewSettings;
 import main.java.opal.pokemon.game.screens.overworld.Camera;
-import main.java.opal.pokemon.game.screens.overworld.characters.player.PlayerView;
 import main.java.opal.pokemon.main.UtilityTool;
 import main.java.opal.pokemon.game.GameController;
 
@@ -59,9 +58,6 @@ public class TileGraphics {
                         worldY <= camera.bottom) {
 
                     g2.drawImage(getTileImage(tileNum), worldX - camera.left, worldY - camera.top, ViewSettings.tileSize, ViewSettings.tileSize, null);
-                    // NOTE: For debugging purposes - make toggleable!
-//                    g2.setColor(new Color(255,0,255));
-//                    g2.drawRect(worldX - camera.left, worldY - camera.top, ViewSettings.tileSize, ViewSettings.tileSize);
                 }
             }
         }
